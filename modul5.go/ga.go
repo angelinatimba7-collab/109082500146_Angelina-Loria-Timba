8eae3b1936d8adc8a69d6c9e441e3e3572e1489f
angelinatimba7-collab <angelinatimba7@gmail.com>
2026-04-09 17:40:18 +0700

    Add files via upload

---
+++ modul5.go/ga.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func faktor(n, i int) {
+	if i > n {
+		return
+	}
+
+	if n%i == 0 {
+		fmt.Print(i, " ")
+	}
+
+	faktor(n, i+1)
+}
+
+func main() {
+	var n int
+	fmt.Print("Masukkan bilangan: ")
+	fmt.Scan(&n)
+
+	fmt.Print("Faktor: ")
+	faktor(n, 1)
+}
